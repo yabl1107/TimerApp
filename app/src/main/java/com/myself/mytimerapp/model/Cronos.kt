c2877558d5407a3119bad018b3108ec9bdc2ae50
Abel <abel07110711@gmail.com>
2025-03-21 13:04:32 -0500

    Initial commit

---
+++ app/src/main/java/com/myself/mytimerapp/model/Cronos.kt
@@ -0,0 +1,15 @@
+package com.myself.mytimerapp.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "cronos")
+data class Cronos(
+    @PrimaryKey(autoGenerate = true)
+    val id : Long = 0,
+    @ColumnInfo(name = "title")
+    val title : String,
+    @ColumnInfo(name="crono")
+    val crono : Long
+)
---
+++ app/src/main/java/com/myself/mytimerapp/room/CronosDatabaseDao.kt
@@ -0,0 +1,4 @@
+package com.myself.mytimerapp.room
+
+interface CronosDatabaseDao {
+}
